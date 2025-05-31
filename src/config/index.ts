1208eece8da736e97b96bfa77babe12d7276b0a5
yoichi.ishigaki <deepestred1968@gmail.com>
2025-05-31 15:02:37 +0900

    firebaseの設定を追加

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+
+type Config = {
+  NEXTAUTH_SECRET: string;
+  NEXTAUTH_URL: string;
+  FIREBASE_ADMIN_CREDENTIALS: string;
+  FIREBASE_CLIENT_CREDENTIALS: string;
+}
+
+const config: Config = {
+  NEXTAUTH_SECRET: String(process.env.AUTH_NEXT),
+  NEXTAUTH_URL: String(process.env.NEXTAUTH_URL),
+  FIREBASE_ADMIN_CREDENTIALS: String(process.env.FIREBASE_ADMIN_CREDENTIALS),
+  FIREBASE_CLIENT_CREDENTIALS: String(process.env.FIREBASE_CLIENT_CREDENTIALS),
+}
+
+export default config;
---
+++ src/lib/firebase.ts
@@ -0,0 +1,75 @@
+import { initializeApp } from 'firebase/app';
+import type { FirebaseOptions,  FirebaseApp } from 'firebase/app';
+import type { Firestore } from 'firebase/firestore';
+import {
+  getAuth as getAuthFirebase,
+  signInWithEmailAndPassword,
+  type Auth,
+  type UserCredential
+} from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+import config from '@/config';
+
+class FirebaseService {
+  private static instance: FirebaseService;
+  private appPromise: Promise<FirebaseApp> | null = null;
+  private dbPromise: Promise<Firestore> | null = null;
+  private authPromise: Promise<Auth> | null = null;
+
+  // private constructor to prevent direct instantiation.
+  private constructor() {}
+
+  public static getInstance(): FirebaseService {
+    if (!FirebaseService.instance) {
+      FirebaseService.instance = new FirebaseService();
+    }
+    return FirebaseService.instance;
+  }
+
+  public getApp(): Promise<FirebaseApp> {
+    if (!this.appPromise) {
+      this.appPromise = (async () => {
+        const firebaseClientConfig: FirebaseOptions = await import(`config-submodule/${config.FIREBASE_CLIENT_CREDENTIALS}`);
+        return initializeApp(firebaseClientConfig);
+      })();
+    }
+    return this.appPromise;
+  }
+
+  public getDB(): Promise<Firestore> {
+    if (!this.dbPromise) {
+      this.dbPromise = (async () => {
+        const app = await this.getApp();
+        return getFirestore(app);
+      })();
+    }
+    return this.dbPromise;
+  }
+
+  public getAuth(): Promise<Auth> {
+    if (!this.authPromise) {
+      this.authPromise = (async () => {
+        const app = await this.getApp();
+        return getAuthFirebase(app);
+      })();
+    }
+    return this.authPromise;
+  }
+
+  public async signInWithEmail(email: string, password: string): Promise<UserCredential | null> {
+    try {
+      const firebaseAuth = await this.getAuth();
+      const userCredential = await signInWithEmailAndPassword(firebaseAuth, email, password);
+      return userCredential;
+    } catch (error) {
+      console.error("Firebase authentication error:", error);
+      return null;
+    }
+  }
+}
+
+const firebaseService = FirebaseService.getInstance();
+
+export const getApp = async () => firebaseService.getApp();
+export const getDB = async () => firebaseService.getDB();
+export const getAuth = async () => firebaseService.getAuth();
