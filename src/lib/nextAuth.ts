4327e6ea5cee8ee0926427f82570bfca09d10f7c
yoichi.ishigaki <deepestred1968@gmail.com>
2025-08-30 17:55:12 +0900

    refactor auth

--- src/lib/nextAuth.ts
+++ src/lib/nextAuth.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth,  { type AuthOptions } from "next-auth"
 import CredentialsProvider from 'next-auth/providers/credentials';
 import config from "@/config";
 import { signInWithEmail } from './auth'
 
-export const handler = NextAuth({
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: 'Credentials',
@@ -12,19 +12,37 @@ export const handler = NextAuth({
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        if(process.env.IS_TEXT === 'true'){
-          if (credentials?.email === 'user@example.com' && credentials?.password === 'password') {
-            return { id: 'user-uid', name: 'test-user', email: 'user@example.com' };
-          }
+        try {
+          if (process.env.IS_TEXT === 'true') {
+            if (credentials?.email === 'user@example.com' && credentials?.password === 'password') {
+              return { id: 'user-uid', name: 'test-user', email: 'user@example.com', role: 'admin' };
+            }
+            return null;
+          } else {
+            return await signInWithEmail(credentials!.email, credentials!.password)
+          } 
+        } catch (error) {
+          console.error(error);
           return null;
-        } else {
-          return await signInWithEmail(credentials!.email, credentials!.password)
-        } 
+        }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return token;
+    },
+    async session({ session, token }) {
+      return session;
+    },
+  },
   pages: {
     signIn: '/ja/login', // カスタムログインページを使う
   },
   secret: config.NEXTAUTH_SECRET,
+} satisfies AuthOptions;
+
+
+export const handler = NextAuth({
+  ...authOptions,
 })
