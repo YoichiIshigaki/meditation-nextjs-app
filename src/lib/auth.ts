2d1da4c58cc299378a18a34cd8463cae7ca01c68
yoichi.ishigaki <deepestred1968@gmail.com>
2025-12-01 00:51:30 +0900

    Enhance authentication functionality with sign out and password reset features
    - Added `signOut` function to handle user sign out with error logging.
    - Implemented `forgetPassword` function to send password reset emails, including error handling and success response.
    - Updated user data retrieval in `signInWithEmail` to include user name and image fallback options.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,29 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, signOut as firebaseSignOut, sendPasswordResetEmail } from 'firebase/auth';
 import type { UserCredential } from 'firebase/auth';
 import { getAuth } from "./firebase"
 import { User } from 'next-auth';
-
-// const errorCode = {}
+import { getNotThrow } from '../models/user';
 
 export const signInWithEmail = async (email: string, password: string): Promise<User | null> => {
   try { 
     const firebaseAuth = await getAuth();
     const userCredential: UserCredential = await signInWithEmailAndPassword(firebaseAuth, email, password);
     
-    // console.dir(userCredential, { depth: null })
-    console.log("user info")
     console.dir(userCredential.user.providerData, { depth: null })
+    
+    // 仮のユーザー名
+    const temporaryUserName =  userCredential.user.displayName || userCredential.user.email?.split('@')[0]
     // const providerUser = userCredential.user.providerData; 
-
+    const userData = await getNotThrow(userCredential.user.uid)
+    const userName = userData ? `${userData.first_name} ${userData.last_name}` : temporaryUserName;
+        
+    console.log(userCredential.user.uid)
+    const altImageUrl = "https://i.pinimg.com/736x/74/61/e4/7461e4ab734d81af34c7982910c2d20e.jpg"
     return { 
       id: userCredential.user.uid,
-      name: userCredential.user.displayName,
+      name: userName,
       email: userCredential.user.email,
-      image: userCredential.user.photoURL
+      image:  userData?.thumbnail_url ?? userCredential.user.photoURL ?? altImageUrl,
     };
 
   } catch (error) {
