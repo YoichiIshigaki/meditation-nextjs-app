84dc33d82ff31ebf2350bbfb2cf9d3eb2f6d347f
yoichi.ishigaki <deepestred1968@gmail.com>
2026-01-03 18:36:35 +0900

    refactor: utils array getList by Id

--- src/repository/utils.ts
+++ src/repository/utils.ts
@@ -3,9 +3,7 @@ import { chunk } from "@/util/array";
 export const getListByChunk = async <T extends { id: string }>(
   Ids: string[],
   getList: (ids: string[]) => Promise<T[]>,
-): Promise<T[]> => {
-  const IdsChunk = chunk(Ids, 10);
-  return await Promise.all(
-    IdsChunk.map(async (ids) => getList(ids)),
-  ).then((list) => list.flat());
-};
+): Promise<T[]> =>
+  Promise.all(chunk(Ids, 10).map(async (ids) => getList(ids))).then((list) =>
+    list.flat(),
+  );
