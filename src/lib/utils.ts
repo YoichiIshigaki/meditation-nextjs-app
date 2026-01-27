7b3a109c4d91cbb1aef27f88d5fe978162b73801
yoichi.ishigaki <deepestred1968@gmail.com>
2026-01-27 22:11:10 +0900

    add: isServer, isClient utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,3 @@
+export const isClient = () => typeof window !== "undefined" && typeof document !== "undefined";
+
+export const isServer = () => typeof window === "undefined" || typeof document === "undefined"; 
